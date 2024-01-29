a7db36883f76d6c6fd02d0431666407a0f9af127
Shivam9870 <ssv2074@gmail.com>
2024-01-29 21:26:59 +0530

    29Jan24

--- code/01basic/logging/test_logs.py
+++ code/01basic/logging/test_logs.py
@@ -10,8 +10,9 @@ import logging
 
 def test_print_the_logs():
     Logger = logging.getLogger(__name__)
+
     # Intentional logging to user
-    Logger.info("This is Information logs")
-    Logger.warning("This is Warning logs")
-    Logger.error("This is Error logs")
-    Logger.critical("This is Critical logs")
+    Logger.info("This is an Information logs")
+    Logger.warning("This is a Warning logs")
+    Logger.error("This is an Error logs")
+    Logger.critical("This is a Critical logs")
